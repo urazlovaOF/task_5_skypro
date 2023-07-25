6d8d20f895411fbebbfe9ef976be4b207ef8c958
Olga Urazlova <olga.urazlova@gatech.global>
2023-07-25 16:21:10 +0300

    Test for the premium calculation

--- calc_premium_test.py
+++ calc_premium_test.py
@@ -2,8 +2,8 @@ from conftest import calculate_premium
 
 
 def test_calculate_premium(checks_param):
-    years_worked, sick_days_last_year, expected_result = checks_param
+    years_worked, year, sick_days_last_year, expected_result = checks_param
     assert (
-            calculate_premium(years_worked, sick_days_last_year) ==
+            calculate_premium(years_worked, year, sick_days_last_year) ==
             expected_result
     )
